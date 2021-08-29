ddbe10ceef771d2e36db0db336e619aa43c4f6f2
naruhide.ueda <neru@naruhide.com>
2021-08-29 16:21:44 +0900

    Added dialogue automation function

--- git_push.sh
+++ git_push.sh
@@ -2,7 +2,16 @@
 
 git add .
 
-read -p "Enter commit message: " COMMIT_MESSAGE
+read -p 'Enter commit message: ' COMMIT_MESSAGE
 git commit -m "$COMMIT_MESSAGE"
 
 git push -u origin master
+set timeout 3
+expect {
+  "Username:" {
+    send "$GIT_USERNAME\n"
+  }
+  "Password:" {
+    send "$GIT_ACCESS_TOKEN\n"
+  }
+}
